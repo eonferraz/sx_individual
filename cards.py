4129d1b3f6152ced264260f883725cdbb30add47
Paulo Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-07-02 18:21:46 -0300

    Update cards.py

--- cards.py
+++ cards.py
@@ -18,8 +18,8 @@ def render_cards(df_fat, df_cart, df_ped, META_MENSAL, hoje):
 
     st.markdown("""
         <style>
-        .card { border-radius: 1px; padding: 12px; margin-bottom: 5px; text-align: center; }
-        .card b { font-size: 30px; }
+        .card { border-radius: 1px; padding: 5px; margin-bottom: 5px; text-align: center; }
+        .card b { font-size: 20px; }
         .card-title { font-size: 16x; display: block; }
         .meta { background-color: #0160A2; color: white; }
         .realizado { background-color: #A0C63F; color: black; }
