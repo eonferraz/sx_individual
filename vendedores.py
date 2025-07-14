10d3501901eb3fc7310480a8069cff7c270ff301
Paulo Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-07-14 04:53:11 -0300

    Update vendedores.py

--- vendedores.py
+++ vendedores.py
@@ -77,7 +77,7 @@ def render_vendedores(df_fat, df_cart):
     col_esq, col_dir = st.columns(2)
 
     def render_coluna(col, lista):
-        headers = ['Emoji', 'Vendedor', 'Meta', 'Carteira', 'Faturado', 'Pendente', '% Atingido']
+        headers = ['Emoji', 'Vendedor', 'Meta', 'Carteira', 'Faturado', 'Pendente', '%']
         with col:
             cols = st.columns([1, 3, 2, 2, 2, 2, 1])
             for i, campo in enumerate(headers):
